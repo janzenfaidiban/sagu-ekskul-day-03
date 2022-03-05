eadc3f88c4881748e40d9d0c7cac95a92cb37f32
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-03-05 11:45:39 +0900

    project 01

---
+++ project01/config.php
@@ -0,0 +1,5 @@
+<?php
+    // Profile variables
+    $first_name = 'Janzen';
+    $last_name = 'Faidiban';
+?>
---
+++ project01/index.php
@@ -0,0 +1,20 @@
+<?php 
+    include('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 02 Project 01</title>
+</head>
+<body>
+
+    First Name : <?php echo $first_name; ?>
+    <br>
+    Last Name : <?php echo $last_name; ?>
+    <br>
+
+</body>
+</html>
